d9e73bd23fdf6f2b2e047366c46590e14340b74c
Ig <payne-xl@mail.ru>
2017-12-12 17:32:58 +0300

    config work

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,26 +2,35 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const path = require('path');
 
+const dir = __dirname + '/..'
+
 const commonConfig = {
   output: {
-    path: path.resolve(__dirname + '/dist/')
+    path: path.resolve(dir + '/dist/')
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loader: 'babel',
-        include: __dirname,
-        exclude: /node_modules/
+        exclude: [
+          /(node_modules|bower_components)/
+        ],
+        include: [
+          dir
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
       },
       {
         test: /\.vue$/,
-        loader: 'vue'
+        use: 'vue-loader'
       },
-      {
-        test: /\.css$/,
-        loader: 'style!less!css'
-      }
+      // {
+      //   test: /\.css$/,
+      //   use: 'style!less!css'
+      // }
     ]
   },
   plugins: [
@@ -36,22 +45,74 @@ const commonConfig = {
   ]
 }
 
-module.exports = [
-  merge(commonConfig, {
-    entry: path.resolve(__dirname + '/src/index.js'),
-    output: {
-      filename: 'vue-pure-parallax.min.js',
-      libraryTarget: 'window',
-      library: 'VuePureParallax'
-    }
-  }),
-  merge(commonConfig, {
-    entry: path.resolve(__dirname + '/src/index.js'),
-    output: {
-      filename: 'vue-pure-parallax.js',
-      libraryTarget: 'umd',
-      library: 'vue-pure-parallax',
-      umdNamedDefine: true
-    }
-  })
-];
+// module.exports = [
+//   merge(commonConfig, {
+//     entry: path.resolve(dir + '/src/index.js'),
+//     output: {
+//       filename: 'vue-pure-parallax.min.js',
+//       libraryTarget: 'window',
+//       library: 'VuePureParallax'
+//     }
+//   }),
+//   // merge(commonConfig, {
+//   //   entry: path.resolve(dir + '/src/index.js'),
+//   //   output: {
+//   //     filename: 'vue-pure-parallax.js',
+//   //     libraryTarget: 'umd',
+//   //     library: 'vue-pure-parallax',
+//   //     umdNamedDefine: true
+//   //   }
+//   // })
+// ];
+
+module.exports = {
+  entry: path.resolve(dir + '/src/index.js'),
+  output: {
+    path: path.resolve(dir + '/dist/'),
+    filename: 'vue-pure-parallax.min.js',
+    libraryTarget: 'window',
+    library: 'VuePureParallax'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [
+          /(node_modules|bower_components)/
+        ],
+        include: [
+          dir
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            {
+              "targets": {
+                "browsers": ["last 2 versions", "ie >= 8"]
+              }
+            }
+          ]
+        }
+      },
+      // {
+      //   test: /\.vue$/,
+      //   use: 'vue-loader'
+      // },
+      // {
+      //   test: /\.css$/,
+      //   use: 'style!less!css'
+      // }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize : true,
+      sourceMap : false,
+      mangle: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+}
